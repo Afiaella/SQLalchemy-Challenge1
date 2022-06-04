ab5acf9b83f6d4dff0eec61ab2421af178360156
Afiaella <85926823+Afiaella@users.noreply.github.com>
2022-06-04 17:06:35 +0100

    Add files via upload

---
+++ SQLalchemy-Challenge1/app.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def Homepage():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    
